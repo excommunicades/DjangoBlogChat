de2688168d4a7c9a2c986ae21e12d1e51b0425bd
Daniil <daniilworkmail49@gmail.com>
2024-11-04 20:20:01 +0200

    endpoints were changed

--- DjangoBlogChat/blog_user/urls.py
+++ DjangoBlogChat/blog_user/urls.py
@@ -11,8 +11,8 @@ from blog_user.views import (
 )
 
 urlpatterns = [
-    path('registration/', Register_User.as_view(), name="user_registration"),
-    path('authorization/', Login_User.as_view(), name="user_authorization"),
+    path('register', Register_User.as_view(), name="user_registration"),
+    path('login', Login_User.as_view(), name="user_authorization"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
