c33f2c556479780cd0d1c8faebbc41408059095c
Daniil <daniilworkmail49@gmail.com>
2024-11-18 19:17:48 +0200

    Bug fixed.

--- DjangoBlogChat/api/settings/urls.py
+++ DjangoBlogChat/api/settings/urls.py
@@ -7,6 +7,6 @@ from api.api_actions.views.posts_views import(
 )
 
 urlpatterns = [
-    path('posts/', PostsCRUD.as_view(), name="create_list_posts"),
-    path('posts/<int:pk>/', PostsCRUD.as_view(), name="retrieve_update_delete_post"),
+    path('posts', PostsCRUD.as_view(), name="create_list_posts"),
+    path('posts/<int:pk>', PostsCRUD.as_view(), name="retrieve_update_delete_post"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- DjangoBlogChat/blog_user/urls.py
+++ DjangoBlogChat/blog_user/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path('register-confirm', Register_Confirm.as_view(), name="user_registration_confirm"),
     path('request-password-recovery', Request_Password_Recovery.as_view(), name="request_password_recovery"),
     path('password-recovery', Password_Recovery.as_view(), name="password_recovery"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
