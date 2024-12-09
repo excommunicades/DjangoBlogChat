3b2fa2cde6b6f8ae7bae14d34d354697ef905c7c
Daniil <daniilworkmail49@gmail.com>
2024-12-09 10:31:49 +0200

    Dynamic room websocket connection was created.

--- DjangoBlogChat/chat/routing.py
+++ DjangoBlogChat/chat/routing.py
@@ -3,6 +3,8 @@ from chat import consumers
 
 
 websocket_urlpatterns =[
-    re_path(r'ws/public_room/$', consumers.PublicConsumer.as_asgi()),
-    re_path(r'ws/chat/notifications/(?P<user_id>\d+)/$', consumers.ChatNotificationConsumer.as_asgi()),
+    re_path(r'ws/public_room$', consumers.PublicConsumer.as_asgi()),
+    re_path(r'ws/chat/notifications/(?P<user_id>\d+)$', consumers.ChatNotificationConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<user1>\w+)/(?P<user2>\w+)$', consumers.ChatConsumer.as_asgi()),
+
 ]
