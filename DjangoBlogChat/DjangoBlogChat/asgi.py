752631550ab3396708ce24396a830bd5b7a14b0e
excommunicades <daniilworkmail49@gmail.com>
2024-12-10 18:50:47 +0200

    Consumer outputs was changed. Project architecture upgraded

--- DjangoBlogChat/DjangoBlogChat/asgi.py
+++ DjangoBlogChat/DjangoBlogChat/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat import routing
+from websocket.chat.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoBlogChat.settings')
@@ -21,7 +21,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
--- DjangoBlogChat/websocket/chat/consumers.py
+++ DjangoBlogChat/websocket/chat/consumers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import redis
 
@@ -224,42 +225,57 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         user_1 = self.scope['url_route']['kwargs']['user1']
         user_2 = self.scope['url_route']['kwargs']['user2']
+
         room_name = f'chat_{min(int(user_1), int(user_2))}_{max(int(user_1), int(user_2))}'
         
         self.room_group_name = room_name
 
-        # Пробуем подключиться к группе, в случае успеха — принимаем соединение
+
         try:
-            # Добавляем пользователя в группу
+
             await self.channel_layer.group_add(
                 self.room_group_name,
                 self.channel_name
             )
-            await self.accept()  # Принимаем соединение
+            await self.accept()
+
         except Exception as e:
-            print(f"Ошибка подключения: {e}")
-            await self.close()  # Закрываем соединение в случае ошибки
+
+            print(f"Connection error-: {e}")
+
+            await self.close()
+
     async def disconnect(self, close_code):
-        # Логика отключения
+
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     async def receive(self, text_data):
-        # Получаем сообщение и отправляем его обратно
+
         text_data_json = json.loads(text_data)
+        sender = text_data_json['senderId']
         message = text_data_json['message']
+        message_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        print('receive_',text_data_json['senderId'])
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'sender': sender,
+                'time': message_time,
+
             }
         )
 
     async def chat_message(self, event):
-        # Отправка сообщения в WebSocket
+        print(event)
         await self.send(text_data=json.dumps({
-            'message': event['message']
+            'message': event['message'],
+            'sender': event['sender'],
+            'time':  event['time'],
         }))
