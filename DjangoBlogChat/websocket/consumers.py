2762a146e5549870ed55901881e2d52eea9e7eea
excommunicades <daniilworkmail49@gmail.com>
2024-12-11 21:58:56 +0200

    Community connection was added!

--- DjangoBlogChat/websocket/consumers.py
+++ DjangoBlogChat/websocket/consumers.py
@@ -3,13 +3,43 @@ import json
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+connected_users = []
+print(connected_users)
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class CommunityConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
 
-        pass
+        self.username = self.scope.get('user', {}).get('username', 'Uknown User')
+
+        connected_users.append(self)
+
+        for user in connected_users:
+                if user != self:
+                    await user.send(text_data=json.dumps({
+                        'message': f'{self.username} connected. Summary connections: {len(connected_users)}'
+                    }))
+                await user.send(text_data=json.dumps({
+                    'message': f'{self.username} connected. Summary connections: {len(connected_users)}'
+                }))
+
+        await  self.accept()
 
     async def disconnect(self):
 
-        pass
+        connected_users.remove(self)
+
+        for user in connected_users:
+            await user.send(text_data=json.dumps({
+                'message': f'{self.username} disconnected. Summary connections: {len(connected_users)}'
+            }))
+
+    async def receive(self, text_data):
+
+        message = json.loads(text_data).get('message', '')
+
+        for user in connected_users:
+            if user != self:
+                await user.send(text_data=json.dumps({
+                    'message': message
+                }))
--- DjangoBlogChat/websocket/routing.py
+++ DjangoBlogChat/websocket/routing.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from websocket.consumers import (
+    CommunityConsumer
+)
 
 websocket_urlpatterns=[
-    
+    path('ws/community', CommunityConsumer.as_asgi()),
 ]
