52c47ccc80d42c093a8be3ad46b4829d34bbe624
Daniil <daniilworkmail49@gmail.com>
2024-12-16 00:39:11 +0200

    Delete chat/ forward message functions was added to consumer!

--- DjangoBlogChat/websocket/consumers.py
+++ DjangoBlogChat/websocket/consumers.py
@@ -1,29 +1,8 @@
-from datetime import datetime
-import asyncio
-import json
-import hashlib
-
-from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-
-from django.db import IntegrityError
-from django.db.models import Max
+from websocket.websocket_community_action import CommunityAction
+import json
 
 connected_users = {}
-
-@database_sync_to_async
-def get_user_by_id(user_id):
-
-    from blog_user.models import BlogUser
-
-    try:
-
-        return BlogUser.objects.get(id=user_id)
-
-    except BlogUser.DoesNotExist:
-
-        return None
-
 
 class CommunityConsumer(AsyncWebsocketConsumer):
 
@@ -58,229 +37,79 @@ class CommunityConsumer(AsyncWebsocketConsumer):
 
         if action == 'chat_message':
 
-            participants = data.get('participants').split(',')
-            sender_id = int(data.get('sender'))
-            sender_name = str(data.get('sender_name'))
-            message = data.get('message')
-
-            participants_ids = list(map(int, participants))
-
-            participants_ids.sort()
-
-            chat_name = f"chat_{'_'.join(map(str, participants_ids))}"
-
-            chat_hash = hashlib.sha256(chat_name.encode('utf-8')).hexdigest()
-
-            for participant_id in participants_ids:
-
-                participant_channel = connected_users.get(participant_id)
-
-                user = await get_user_by_id(int(participant_id))
-
-                await self.add_user_to_chat(chat_name=chat_hash, user=user)
-                
-                if participant_channel:
-                    await participant_channel.send(text_data=json.dumps({
-                        'type': 'chat_message',
-                        'sender_id': sender_id,
-                        'username': sender_name,
-                        'message': message,
-                    }))
-
-            user = await get_user_by_id(int(self.user_id))
-
-            await self.save_message_to_chat(chat_hash, user, message)
+            await CommunityAction.chat_message(data, self.user_id, connected_users)
 
         elif action == 'get_chat_list':
 
-            user_chats = await self.get_user_chats(self.user_id)
-
-            await self.send(text_data=json.dumps({
-                'type': 'chat_list',
-                'chats': user_chats
-            }))
+            result = await CommunityAction.get_chat_list(self.user_id)
+            await self.send(text_data=json.dumps(result))
 
         elif action == 'get_chat_messages':
 
             chat_id = data.get('chat_id')
+            result = await CommunityAction.get_chat_messages(chat_id)
 
-            messages = await self.get_chat_messages(chat_id)
+            await self.send(text_data=json.dumps(result))
 
-            await self.send(text_data=json.dumps({
-                'type': 'chat_messages',
-                'messages': messages
-            }))
+        elif action == 'delete_chat':
+
+            chat_id = data.get('chat_id')
+
+            participants = await CommunityAction.delete_chat(chat_id, self.user_id)
+
+            if participants:
+
+                for participant_channel in participants:
+                    await participant_channel.send(text_data=json.dumps({
+                        'type': 'message_deleted',
+                        'message_id': message_id,
+                    }))
 
         elif action == 'delete_chat_message':
 
             message_id = data.get('message_id')
+            participants = await CommunityAction.delete_chat_message(message_id, self.user_id)
 
-            await self.send_delete_message(message_id=message_id,user=self.user_id)
+            for participant_channel in participants:
+                await participant_channel.send(text_data=json.dumps({
+                    'type': 'message_deleted',
+                    'message_id': message_id,
+                }))
 
         elif action == 'update_chat_message':
 
             message_id = data.get('message_id')
-            
             new_message_content = data.get('new_message_content')
+            participants = await CommunityAction.update_chat_message(message_id, self.user_id, new_message_content)
 
-            await self.send_update_message(message_id=message_id, user=self.user_id, new_message_content=new_message_content)
+            for participant_channel in participants:
+                await participant_channel.send(text_data=json.dumps({
+                    'type': 'message_updated',
+                    'message_id': message_id,
+                    'new_content': new_message_content,
+                }))
+
+        elif action == 'forward_message':
+
+            message_content = data.get('message_content')
+            from_user_id = data.get('from_user_id')
+            from_user_username = data.get('from_user_username')
+            to_chat_id = data.get('to_user_id')
+
+            participants = await CommunityAction.forward_chat_message(message_content, from_user_id, to_chat_id)
+
+            for participant_channel in participants:
+
+                await participant_channel.send(text_data=json.dumps({
+                    'type': 'forward_message',
+                    'message_content': message_content,
+                    'from_user_username': from_user_username, 
+                }))
 
 
     async def chat_message(self, event):
-
         await self.send(text_data=json.dumps({
             'type': 'chat_message',
             'user_id': event['user_id'],
             'message': event['message']
         }))
-
-
-    @database_sync_to_async
-    def add_user_to_chat(self, chat_name, user):
-
-        from publish.models import ChatRoom
-
-        chat, created = ChatRoom.objects.get_or_create(name=chat_name)
-
-        if user not in chat.users.values_list('id', flat=True):
-            chat.users.add(user)
-
-        return chat
-
-
-    @database_sync_to_async
-    def save_message_to_chat(self, chat_name, user, message):
-
-        from publish.models import ChatRoom, Message
-
-        chat = ChatRoom.objects.get(name=chat_name)
-
-        Message.objects.create(
-            user=user,
-            room=chat,
-            content=message
-        )
-
-    @database_sync_to_async
-    def get_user_chats(self, user_id):
-
-        from publish.models import ChatRoom
-
-        chats = ChatRoom.objects.filter(users__id=user_id)
-
-        chats = chats.annotate(last_message_time=Max('messages__timestamp'))
-
-        chats = chats.order_by('-last_message_time')
-
-        chat_list = []
-
-        for chat in chats:
-
-            users_in_chat = chat.users.exclude(id=user_id)
-            
-            user_names = [user.username for user in users_in_chat]
-
-            chat_users = ', '.join(user_names)
-
-            last_message_time = chat.last_message_time.isoformat() if chat.last_message_time else None
-
-            chat_list.append({
-                'chat_users': chat_users,
-                'chat_id': chat.id,
-                'last_message_time': last_message_time
-            })
-
-        return chat_list
-
-
-    @database_sync_to_async
-    def get_chat_messages(self, chat_id):
-
-        from publish.models import Message
-
-        messages = Message.objects.filter(room_id=chat_id).select_related('user').order_by('-timestamp')
-
-        message_list = [{
-                'message_id': message.id,
-                'user_id': message.user.id,
-                'username': message.user.username,
-                'message': message.content,
-                'timestamp': message.timestamp.isoformat()} for message in messages]
-
-        return message_list
-
-
-    @database_sync_to_async
-    def delete_message(self,message_id, user):
-
-        from publish.models import Message
-
-        message = Message.objects.filter(id=message_id).first()
-
-        if message and message.user.id == int(user):
-
-            chat = message.room
-            message.delete()
-
-            participants = chat.users.all()
-
-            participants_id_list = []
-
-            for participant in participants:
-
-                if connected_users.get(participant.id) is not None:
-
-                    participants_id_list.append(connected_users.get(participant.id))
-
-        return participants_id_list
-
-    async def send_delete_message(self, message_id, user):
-
-        participants_id_list = await self.delete_message(message_id, user)
-
-        for participant_channel in participants_id_list:
-
-            await participant_channel.send(text_data=json.dumps({
-                'type': 'message_deleted',
-                'message_id': message_id,
-            }))
-    
-
-
-    @database_sync_to_async
-    def update_message(self, message_id, user, new_message_content):
-
-        from publish.models import Message
-
-        message = Message.objects.filter(id=message_id).first()
-
-        if message and message.user.id == int(user):
-
-            message.content = new_message_content
-            message.save()
-
-            chat = message.room
-
-            participants = chat.users.all()
-
-            participants_id_list = []
-
-            for participant in participants:
-
-                if connected_users.get(participant.id) is not None:
-
-                    participants_id_list.append(connected_users.get(participant.id))
-
-        return participants_id_list
-
-    async def send_update_message(self, message_id, user, new_message_content):
-
-        participants_id_list = await self.update_message(message_id, user, new_message_content)
-
-        for participant_channel in participants_id_list:
-
-            await participant_channel.send(text_data=json.dumps({
-                'type': 'message_updated',
-                'message_id': message_id,
-                'new_content': new_message_content,
-            }))
---
+++ DjangoBlogChat/websocket/websocket_community_action.py
@@ -0,0 +1,100 @@
+import hashlib
+import json
+
+from websocket.websocket_database_helpers import (
+    get_user_by_id,
+    add_user_to_chat,
+    save_message_to_chat,
+    get_user_chats,
+    get_chat_messages,
+    delete_message,
+    update_message,
+    save_forward_message,
+    delete_chat,
+)
+
+
+class CommunityAction:
+
+    @staticmethod
+    async def chat_message(data, user_id, connected_users):
+
+        participants = data.get('participants').split(',')
+        sender_id = int(data.get('sender'))
+        sender_name = str(data.get('sender_name'))
+        message = data.get('message')
+
+        participants_ids = list(map(int, participants))
+        participants_ids.sort()
+
+        chat_name = f"chat_{'_'.join(map(str, participants_ids))}"
+        chat_hash = hashlib.sha256(chat_name.encode('utf-8')).hexdigest()
+
+        for participant_id in participants_ids:
+
+            participant_channel = connected_users.get(participant_id)
+            
+            user = await get_user_by_id(int(participant_id))
+
+            await add_user_to_chat(chat_name=chat_hash, user=user)
+
+            if participant_channel:
+
+                await participant_channel.send(text_data=json.dumps({
+                    'type': 'chat_message',
+                    'sender_id': sender_id,
+                    'username': sender_name,
+                    'message': message,
+                }))
+
+        # Save the message to the chat
+        user = await get_user_by_id(int(user_id))
+        await save_message_to_chat(chat_hash, user, message)
+
+    @staticmethod
+    async def get_chat_list(user_id):
+
+        user_chats = await get_user_chats(user_id)
+
+        return {
+            'type': 'chat_list',
+            'chats': user_chats
+        }
+
+    @staticmethod
+    async def get_chat_messages(chat_id):
+
+        messages = await get_chat_messages(chat_id)
+
+        return {
+            'type': 'chat_messages',
+            'messages': messages
+        }
+
+    @staticmethod
+    async def delete_chat(chat_id, user):
+
+        participants_id_list = await delete_chat(chat_id, user)
+
+        return participants_id_list
+
+    @staticmethod
+    async def delete_chat_message(message_id, user):
+
+        participants_id_list = await delete_message(message_id, user)
+
+        return participants_id_list
+
+    @staticmethod
+    async def update_chat_message(message_id, user, new_message_content):
+
+        participants_id_list = await update_message(message_id, user, new_message_content)
+
+        return participants_id_list
+
+    @staticmethod
+    async def forward_chat_message(message_content, from_user_id, to_chat_id):
+
+        participants_id_list = await save_forward_message(message_content, from_user_id, to_chat_id)
+
+        return participants_id_list
